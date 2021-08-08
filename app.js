775eb4e7154cb680b1e6919b5c6032ca70a0d9dc
Ayodeji adeyeye <adeyeye17@gmail.com>
2021-08-08 21:27:34 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+
+// traversing the dom
+
+
+
+// const btns = document.querySelectorAll(".question-btn")
+
+// btns.forEach((bt)=>{
+//     bt.addEventListener("click", (e)=>{
+//        const question = e.currentTarget.parentElement.parentElement;
+        
+//             question.classList.toggle("show-text")
+//     })
+
+// })
+
+
+
+
+
+
+//using selectors inside the element
+const questions = document.querySelectorAll(".question")
+
+questions.forEach((question)=>{
+   const btn = question.querySelector(".question-btn")
+    btn.addEventListener("click", ()=>{
+
+
+        questions.forEach((items)=>{
+            if (items !== question){
+                items.classList.remove("show-text")
+            };
+        });
+
+        question.classList.toggle("show-text")
+    })
+    });
